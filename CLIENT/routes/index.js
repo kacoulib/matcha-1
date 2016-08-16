9e88153280b7f7746d5d01dafa5e6738768df80d
Yoann FUKS <yfuks@student.42.fr>
2016-08-16 20:56:39 +0200

    1

--- CLIENT/routes/index.js
+++ CLIENT/routes/index.js
@@ -2,12 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var path = require("path");
-	res.sendFile(path.join(__dirname, '..', 'CLIENT' ,'index.html'));
-});
-
-router.route('/', function(req, res, next) {
+router.get('/', function(req, res) {
   var path = require("path");
 	res.sendFile(path.join(__dirname, '..', 'CLIENT' ,'index.html'));
 });
