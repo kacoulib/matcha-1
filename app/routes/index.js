58bfbeada67efd98cdc656680c32e498aa200dd4
Yoann FUKS <yfuks@student.42.fr>
2016-09-28 18:37:46 +0200

    a

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,4 +5,8 @@ router.get('/', [], function (req, res) {
   return res.render('index', {});
 });
 
+router.get('/signup', [], function (req, res) {
+  return res.render('signup', {});
+});
+
 module.exports = router;
