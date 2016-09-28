4a72cf6077a6bb2ea8787c38f9f756c457dc2e14
Yoann FUKS <yfuks@student.42.fr>
2016-09-28 16:48:27 +0200

    add 404 and landing

--- app.js
+++ app.js
@@ -27,8 +27,23 @@ app.use(require('./app/routes/index'));
 app.use(require('./app/authentication/login'));
 
 // 404 not found
-app.use(function (req, res, next) {
-  res.status(404).send('<html><h1>404 page not found</h1></html>');
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
 });
 
 // 500 internal
